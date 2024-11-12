7df816a0e431ae3050220df757ba06b0fa876b66
Marek Belisko <marek.belisko@gmail.com>
2024-11-12 21:13:46 +0100

    Config build changes
    Signed-off-by: Marek Belisko <marek.belisko@gmail.com>

--- build_image.sh
+++ build_image.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 CONFIG=kas/imx93-voipac.yml
+KIT_CONFIG="$1"
 
-if  [ ! -z "${1}" ]; then
-	kas shell ${CONFIG} -c "$1"
+
+if [ -z "${KIT_CONFIG}" ]; then
+	echo "Unknown config, using lite ..."
+	KIT_CONFIG="lite"
+fi
+
+case ${KIT_CONFIG} in
+	"lite" )
+		echo "Lite configuration"
+		CONFIG="${CONFIG}:kas/imx93-config-lite.yml";;
+	"basic" )
+		echo "Basic configuration"
+		CONFIG="${CONFIG}:kas/imx93-config-basic.yml";;
+	"pro" )
+		echo "Pro configuration"
+		CONFIG="${CONFIG}:kas/imx93-config-pro-max.yml";;
+	"max" )
+		echo "Max configuration"
+		CONFIG="${CONFIG}:kas/imx93-config-pro-max.yml";;
+esac
+
+
+if  [ ! -z "${2}" ]; then
+	kas shell ${CONFIG} -c "$2"
 else
 	kas build ${CONFIG}
 fi
